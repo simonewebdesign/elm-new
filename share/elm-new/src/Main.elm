8ba10913f808940f9169f21974fa8056c68c6889
Simone Vittori <hello@simonewebdesign.it>
2016-05-31 09:15:43 +0100

    Add src/Main.elm (Html.program)

---
+++ share/elm-new/src/Main.elm
@@ -0,0 +1,62 @@
+import Html exposing (..)
+import Html.App as Html
+--import Html.Attributes exposing (..)
+--import Html.Events exposing (onClick)
+
+
+main : Program Never
+main =
+  Html.program
+    { init = init
+    , view = view
+    , update = update
+    , subscriptions = subscriptions
+    }
+
+
+-- MODEL
+
+type alias Model =
+  {
+  }
+
+
+initialModel : Model
+initialModel =
+  {
+  }
+
+
+init : (Model, Cmd Msg)
+init =
+  (initialModel, Cmd.none)
+
+
+-- UPDATE
+
+type Msg
+  = NoOp
+
+
+update : Msg -> Model -> (Model, Cmd Msg)
+update msg model =
+  case msg of
+    NoOp ->
+      (model, Cmd.none)
+
+
+-- SUBSCRIPTIONS
+
+subscriptions : Model -> Sub Msg
+subscriptions model =
+  Sub.none
+
+
+-- VIEW
+
+view : Model -> Html Msg
+view model =
+  div []
+    [ text "Hello, world!"
+    , text (toString model)
+    ]
