8b33f18129cfc821c9cbf9945a95b02133db300f
Simone Vittori <hello@simonewebdesign.it>
2016-08-19 16:13:34 +0100

    Write more tests

--- test.sh
+++ test.sh
@@ -13,7 +13,7 @@ function fail {
 
 # Print test title
 function test {
-  echo -n $1
+  echo -n "$1"
 }
 
 # Print debug info
@@ -39,6 +39,14 @@ if [[ $($cli -h) == "usage:"* ]]; then ok; else fail; fi
 
 test "prints version (-V|--version)"
 if [ "$($cli -V)" == "1.1.0" ]; then ok; else fail; fi
+
+echo When invalid arg provided,
+test "    prints to stderr"
+if [[ "$($cli --badarg 2>&1)" =~ "elm-new: illegal option --badarg" ]]; then ok; else fail; fi
+
+test "    prints synopsis to stdout"
+if [[ "$($cli --badarg 2>/dev/null)" =~ "usage:" ]]; then ok; else fail; fi
+
 
 sandbox
 debug "Running $cli with no args..."
@@ -67,6 +75,7 @@ if [ ! -f "src/MainHelloWorld.elm" ]; then ok; else fail; fi
 
 test "It's an Html.program"
 if grep --quiet Html.program src/Main.elm; then ok; else fail; fi
+
 
 clean
 sandbox
@@ -97,6 +106,7 @@ if [ ! -f "src/MainHelloWorld.elm" ]; then ok; else fail; fi
 test "It's an Html.beginnerProgram"
 if grep --quiet Html.beginnerProgram src/Main.elm; then ok; else fail; fi
 
+
 clean
 sandbox
 debug "Running $cli --hello-world..."
@@ -125,6 +135,36 @@ if [ ! -f "src/MainHelloWorld.elm" ]; then ok; else fail; fi
 
 test "It's a 'Hello world' program"
 if grep --quiet "Hello, World!" src/Main.elm; then ok; else fail; fi
+
+
+clean
+sandbox
+debug "Running $cli somepath --hello-world --beginner..."
+../$cli somepath --hello-world --beginner >/dev/null
+
+test "elm-package.json exists"
+if [ -f "somepath/elm-package.json" ]; then ok; else fail; fi
+
+test "README.md exists"
+if [ -f "somepath/README.md" ]; then ok; else fail; fi
+
+test ".gitignore exists"
+if [ -f "somepath/.gitignore" ]; then ok; else fail; fi
+
+test "src folder exists"
+if [ -d "somepath/src" ]; then ok; else fail; fi
+
+test "src/Main.elm exists"
+if [ -f "somepath/src/Main.elm" ]; then ok; else fail; fi
+
+test "src/MainBeginner.elm does not exist"
+if [ ! -f "somepath/src/MainBeginner.elm" ]; then ok; else fail; fi
+
+test "src/MainHelloWorld.elm does not exist"
+if [ ! -f "somepath/src/MainHelloWorld.elm" ]; then ok; else fail; fi
+
+test "It's an Html.beginnerProgram"
+if grep --quiet Html.beginnerProgram somepath/src/Main.elm; then ok; else fail; fi
 
 clean
 
