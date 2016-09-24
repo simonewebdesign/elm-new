5b00164210c83321c7e3b393377ec28d3f5422e3
Simone Vittori <hello@simonewebdesign.it>
2016-09-24 22:24:48 +0100

    wip

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,16 @@
 use std::env;
 use std::io::Write;
+// use std::fs;
 
 fn main() {
+    let mut path = ".";
+
     for arg in env::args() {
         match &arg as &str {
+            // temporary solution; should ignore the first argument
+            "target/debug/elm-new" => {
+                continue;
+            }
 
             "-h" | "--help" => {
                 println!("usage: elm-new [-V|--version] [-h|--help] [-b|--beginner] [--hello-world] [path]");
@@ -14,18 +21,35 @@ fn main() {
             }
 
             arg => {
-                if arg.starts_with('-') {
+                if arg.starts_with('-') { // arg is bad
                     std::io::stderr()
-                        .write(b"elm-new: illegal option --badarg")
+                        .write(b"elm-new: illegal option --badarg\n")
                         .ok()
                         .expect("could not write to stderr");
 
                     println!("usage: elm-new [-V|--version] [-h|--help] [-b|--beginner] [--hello-world] [path]");
+                    std::process::exit(1);
+                } else { // arg is path
+                    println!("arg is a path: {}", arg);
+                    path = arg;
+                    // println!("`mkdir a`");
+                    // Create a directory, returns `io::Result<()>`
+                    // match fs::create_dir(arg) {
+                    //     Err(why) => println!("! {:?}", why.kind()),
+                    //     Ok(_) => {},
+                    // }
+
+                    // println!("`echo hello > a/b.txt`");
+                    // // The previous match can be simplified using the `unwrap_or_else` method
+                    // echo("hello", &Path::new("a/b.txt")).unwrap_or_else(|why| {
+                    //     println!("! {:?}", why.kind());
+                    // });
                 }
 
                 continue;
-                // std::process::exit(0);
             }
         }
     }
+
+    println!("path was set to {}", path);
 }
