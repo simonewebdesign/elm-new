67cce525c3b2f624a33061ebe505840fe54a80a3
Simone Vittori <hello@simonewebdesign.it>
2016-10-01 13:00:30 +0100

    Fix installation script

--- install.bat
+++ install.bat
@@ -7,7 +7,17 @@ echo Downloading...
 powershell -Command "(New-Object Net.WebClient).DownloadFile('https://codeload.github.com/simonewebdesign/elm-new/zip/v1.1.1a', 'C:\Tools\elm-new\elm-new.zip')"
 
 echo Extracting...
-call :unzip "C:\Tools\elm-new\extracted" "C:\Tools\elm-new\elm-new.zip"
+call :unzip "C:\Tools\elm-new" "C:\Tools\elm-new\elm-new.zip"
+
+echo Installing...
+robocopy C:\Tools\elm-new\elm-new-1.1.1a C:\Tools\elm-new /e >nul 2>&1
+
+echo Cleaning...
+del C:\Tools\elm-new\elm-new.zip
+rmdir /s /q C:\Tools\elm-new\elm-new-1.1.1a
+
+echo Done!
+exit /b
 
 :unzip <ExtractTo> <newzipfile>
 set vbs="%temp%\_.vbs"
@@ -23,12 +33,3 @@ if exist %vbs% del /f /q %vbs%
 >>%vbs% echo Set objShell = Nothing
 cscript //nologo %vbs%
 if exist %vbs% del /f /q %vbs%
-
-echo Installing...
-move C:\Tools\elm-new\extracted\elm-new-1.1.1a\elm-new.bat C:\Tools\elm-new\elm-new.bat
-
-echo Cleaning...
-rmdir /s /q C:\Tools\elm-new\extracted
-
-echo Done!
-exit /b
