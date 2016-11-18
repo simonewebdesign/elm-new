f97070541738cfa1a019febc8d63eedce519c70d
Simone Vittori <hello@simonewebdesign.it>
2016-11-18 17:59:50 +0000

    Update docs and tests

--- test.sh
+++ test.sh
@@ -41,10 +41,10 @@ test "prints help (--help)"
 if [[ $($cli --help) == "usage:"* ]]; then ok; else fail; fi
 
 test "prints version (-V)"
-if [ "$($cli -V)" == "1.2.0" ]; then ok; else fail; fi
+if [ "$($cli -V)" == 1.2.0 ]; then ok; else fail; fi
 
 test "prints version (--version)"
-if [ "$($cli --version)" == "1.2.0" ]; then ok; else fail; fi
+if [ "$($cli --version)" == 1.2.0 ]; then ok; else fail; fi
 
 echo When invalid arg provided,
 test "    prints to stderr"
@@ -62,25 +62,25 @@ debug "Running $cli with no args..."
 ../$cli >/dev/null
 
 test "elm-package.json exists"
-if [ -f "elm-package.json" ]; then ok; else fail; fi
+if [ -f elm-package.json ]; then ok; else fail; fi
 
 test "README.md exists"
-if [ -f "README.md" ]; then ok; else fail; fi
+if [ -f README.md ]; then ok; else fail; fi
 
 test ".gitignore exists"
-if [ -f ".gitignore" ]; then ok; else fail; fi
+if [ -f .gitignore ]; then ok; else fail; fi
 
-test "src folder exists"
-if [ -d "src" ]; then ok; else fail; fi
+test "src/ exists"
+if [ -d src ]; then ok; else fail; fi
 
 test "src/Main.elm exists"
-if [ -f "src/Main.elm" ]; then ok; else fail; fi
+if [ -f src/Main.elm ]; then ok; else fail; fi
 
 test "src/MainBeginner.elm does not exist"
-if [ ! -f "src/MainBeginner.elm" ]; then ok; else fail; fi
+if [ ! -f src/MainBeginner.elm ]; then ok; else fail; fi
 
 test "src/MainHelloWorld.elm does not exist"
-if [ ! -f "src/MainHelloWorld.elm" ]; then ok; else fail; fi
+if [ ! -f src/MainHelloWorld.elm ]; then ok; else fail; fi
 
 test "It's an Html.program"
 if grep --quiet Html.program src/Main.elm; then ok; else fail; fi
@@ -92,25 +92,25 @@ debug "Running $cli --beginner..."
 ../$cli --beginner >/dev/null
 
 test "elm-package.json exists"
-if [ -f "elm-package.json" ]; then ok; else fail; fi
+if [ -f elm-package.json ]; then ok; else fail; fi
 
 test "README.md exists"
-if [ -f "README.md" ]; then ok; else fail; fi
+if [ -f README.md ]; then ok; else fail; fi
 
 test ".gitignore exists"
-if [ -f ".gitignore" ]; then ok; else fail; fi
+if [ -f .gitignore ]; then ok; else fail; fi
 
-test "src folder exists"
-if [ -d "src" ]; then ok; else fail; fi
+test "src/ exists"
+if [ -d src ]; then ok; else fail; fi
 
 test "src/Main.elm exists"
-if [ -f "src/Main.elm" ]; then ok; else fail; fi
+if [ -f src/Main.elm ]; then ok; else fail; fi
 
 test "src/MainBeginner.elm does not exist"
-if [ ! -f "src/MainBeginner.elm" ]; then ok; else fail; fi
+if [ ! -f src/MainBeginner.elm ]; then ok; else fail; fi
 
 test "src/MainHelloWorld.elm does not exist"
-if [ ! -f "src/MainHelloWorld.elm" ]; then ok; else fail; fi
+if [ ! -f src/MainHelloWorld.elm ]; then ok; else fail; fi
 
 test "It's an Html.beginnerProgram"
 if grep --quiet Html.beginnerProgram src/Main.elm; then ok; else fail; fi
@@ -122,25 +122,25 @@ debug "Running $cli --hello-world..."
 ../$cli --hello-world >/dev/null
 
 test "elm-package.json exists"
-if [ -f "elm-package.json" ]; then ok; else fail; fi
+if [ -f elm-package.json ]; then ok; else fail; fi
 
 test "README.md exists"
-if [ -f "README.md" ]; then ok; else fail; fi
+if [ -f README.md ]; then ok; else fail; fi
 
 test ".gitignore exists"
-if [ -f ".gitignore" ]; then ok; else fail; fi
+if [ -f .gitignore ]; then ok; else fail; fi
 
-test "src folder exists"
-if [ -d "src" ]; then ok; else fail; fi
+test "src/ exists"
+if [ -d src ]; then ok; else fail; fi
 
 test "src/Main.elm exists"
-if [ -f "src/Main.elm" ]; then ok; else fail; fi
+if [ -f src/Main.elm ]; then ok; else fail; fi
 
 test "src/MainBeginner.elm does not exist"
-if [ ! -f "src/MainBeginner.elm" ]; then ok; else fail; fi
+if [ ! -f src/MainBeginner.elm ]; then ok; else fail; fi
 
 test "src/MainHelloWorld.elm does not exist"
-if [ ! -f "src/MainHelloWorld.elm" ]; then ok; else fail; fi
+if [ ! -f src/MainHelloWorld.elm ]; then ok; else fail; fi
 
 test "It's a 'Hello world' program"
 if grep --quiet "Hello, World!" src/Main.elm; then ok; else fail; fi
@@ -152,28 +152,58 @@ debug "Running $cli somepath --hello-world --beginner..."
 ../$cli somepath --hello-world --beginner >/dev/null
 
 test "elm-package.json exists"
-if [ -f "somepath/elm-package.json" ]; then ok; else fail; fi
+if [ -f somepath/elm-package.json ]; then ok; else fail; fi
 
 test "README.md exists"
-if [ -f "somepath/README.md" ]; then ok; else fail; fi
+if [ -f somepath/README.md ]; then ok; else fail; fi
 
 test ".gitignore exists"
-if [ -f "somepath/.gitignore" ]; then ok; else fail; fi
+if [ -f somepath/.gitignore ]; then ok; else fail; fi
 
-test "src folder exists"
-if [ -d "somepath/src" ]; then ok; else fail; fi
+test "src/ exists"
+if [ -d somepath/src ]; then ok; else fail; fi
 
 test "src/Main.elm exists"
-if [ -f "somepath/src/Main.elm" ]; then ok; else fail; fi
+if [ -f somepath/src/Main.elm ]; then ok; else fail; fi
 
 test "src/MainBeginner.elm does not exist"
-if [ ! -f "somepath/src/MainBeginner.elm" ]; then ok; else fail; fi
+if [ ! -f somepath/src/MainBeginner.elm ]; then ok; else fail; fi
 
 test "src/MainHelloWorld.elm does not exist"
-if [ ! -f "somepath/src/MainHelloWorld.elm" ]; then ok; else fail; fi
+if [ ! -f somepath/src/MainHelloWorld.elm ]; then ok; else fail; fi
 
 test "It's an Html.beginnerProgram"
 if grep --quiet Html.beginnerProgram somepath/src/Main.elm; then ok; else fail; fi
+
+
+clean
+sandbox
+debug "Running $cli somepath --navigation..."
+../$cli somepath --navigation >/dev/null
+
+test "elm-package.json exists"
+if [ -f somepath/elm-package.json ]; then ok; else fail; fi
+
+test "README.md exists"
+if [ -f somepath/README.md ]; then ok; else fail; fi
+
+test ".gitignore exists"
+if [ -f somepath/.gitignore ]; then ok; else fail; fi
+
+test "src/ exists"
+if [ -d somepath/src ]; then ok; else fail; fi
+
+test "src/Main.elm exists"
+if [ -f somepath/src/Main.elm ]; then ok; else fail; fi
+
+test "src/MainBeginner.elm does not exist"
+if [ ! -f somepath/src/MainBeginner.elm ]; then ok; else fail; fi
+
+test "src/MainHelloWorld.elm does not exist"
+if [ ! -f somepath/src/MainHelloWorld.elm ]; then ok; else fail; fi
+
+test "It's a Navigation.program"
+if grep --quiet Navigation.program somepath/src/Main.elm; then ok; else fail; fi
 
 clean
 
